0c50a900c65ca7a5657c05f4e60e0c81cdb4aae5
unknown <tamnguyen121999@gmail.com>
2021-07-25 22:26:04 +0700

    Complete an introduction for javascript

--- Basic_Javascript/Introduction/introduction.js
+++ Basic_Javascript/Introduction/introduction.js
@@ -1,8 +1,39 @@
-let name;
-console.log(name);
-console.log("because there is no value for this variable");
-
+// Variables
 let firtName = "Tam",
   lastName = "Nguyen";
 
 console.log(firtName, lastName);
+
+// Constant
+const number1 = 3;
+
+console.log(number1);
+
+// Primative Type
+let name = "Tam";
+let age = 22;
+let isDeveloper = true;
+let hobby = undefined;
+let favoriteColor = null;
+
+// Object
+let person = {
+  name: "Tam",
+  age: 22,
+};
+
+person["age"] = 25;
+
+console.log(person);
+console.log(person.age);
+
+//Array
+let color = ["blue", "red"];
+color[2] = 2;
+console.log(color);
+
+//Function
+function greet(name) {
+  console.log("Hello " + name);
+}
+greet("Tam");
