186f696f135ead21a8305118895a2e338d7751a6
unknown <tamnguyen121999@gmail.com>
2021-09-01 21:12:48 +0700

    Complete style for navbar

--- Project_for_JavaScript/Hard_Project/SPA_path/front_end/static/js/views/UI.js
+++ Project_for_JavaScript/Hard_Project/SPA_path/front_end/static/js/views/UI.js
@@ -1,15 +1,15 @@
 // Hover navbar
 export default class UI {
   focusNavItem(path) {
-    const navItems = Array.from(document.querySelector(".navbar").children);
+    const navItems = document.querySelectorAll("[data-tab-content]");
     navItems.forEach((item) => {
       const href = item.getAttribute("href");
-      item.classList.remove("navbar-item");
+      item.style.setProperty("background-color", "black");
+      item.style.setProperty("color", "white");
 
       if (href === path) {
         item.style.setProperty("background-color", "white");
         item.style.setProperty("color", "black");
-        console.log(item.style);
       }
     });
   }
